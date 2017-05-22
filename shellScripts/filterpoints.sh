6334b090044171d3d8a078d1403ca0a40e45075f
squeakus <jonathanbyrn@gmail.com>
2017-05-22 10:40:14 +0100

    adding improvements to filter script

--- shellScripts/filterpoints.sh
+++ shellScripts/filterpoints.sh
@@ -2,5 +2,5 @@
 
 mkdir filtered
 
-find . -name "*.laz" | xargs -n1 -P3 -I fname pdal pipeline noisefilter.json --writers.las.filename=filtered/fname --readers.las.filename=fname
+find . -maxdepth 1 -name "*.laz" | xargs -n1 -P3 -I fname sh -c 'echo fname; pdal -v 4 pipeline noisefilter.json --writers.las.filename=filtered/fname --readers.las.filename=fname'
 
