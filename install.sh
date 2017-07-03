950520fae9f5cd71e4fe681b7a8a88a9664f805a
squeakus <jonathanbyrn@gmail.com>
2017-07-03 16:50:34 +0100

    fixing up installer

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-sudo add-apt-repository ppa:xorg-edgers/ppa
+sudo add-apt-repository -y ppa:xorg-edgers/ppa
+sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
-aptitude search python
-sudo apt-get install aptitude git subversion vim compizconfig-settings-manager compiz-plugins compiz-plugins-extra
-sudo aptitude install nodejs npm
-sudo aptitude install openssh-server
-sudo aptitude install fail2ban
-sudo aptitude install indicator-multiload
-sudo apt-get install apache2
-sudo apt-get install mysql-server php5-mysql
-sudo aptitude install ipython
-sudo apt-get install libgnome2-bin
+sudo apt-get -y install aptitude git subversion vim compizconfig-settings-manager compiz-plugins compiz-plugins-extra
+sudo aptitude -y install nodejs npm
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo npm install -g -y http-server
+sudo aptitude -y install openssh-server
+sudo aptitude -y install fail2ban
+sudo aptitude -y install indicator-multiload
+#sudo apt-get -y install apache2
+#sudo apt-get install mysql-server php5-mysql
+sudo aptitude -y install ipython
+#sudo apt-get install libgnome2-bin
 
-svn co http://ncra.ucd.ie/svn/Jonathan/programs/bashrc
 cd bashrc/
 sudo make install
+cd ../
 
-alias npython="ipython notebook --pylab inline"
-alias pyserver='python -m SimpleHTTPServer 8080 &'
-alias cmd='vi ~/Jonathan/Documents/commands.txt'
-alias ppython='ipython --pylab'
-alias open="gvfs-open" 
-
+echo "alias npython='ipython notebook --pylab inline'" >> ~/.bashrc.alias
+echo "alias pyserver='python -m SimpleHTTPServer 8080 &'" >> ~/.bashrc.alias
+echo "alias cmd='vi ~/code/bitsandbytes/bashrc/commands.txt'" >> ~/.bashrc.alias
+echo "alias ppython='ipython --pylab'" >> ~/.bashrc.alias
+echo "alias open='gvfs-open'" >> ~/.bashrc.alias
+echo "alias python='python3'" >> ~/.bashrc.alias
 #history | cut -c 8- > install.txt
