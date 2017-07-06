c2d7cadeaf1dd7abc19033ca763f6610ece25b22
squeakus <jonathanbyrn@gmail.com>
2017-07-06 11:14:35 +0100

    fixing install

--- install.sh
+++ install.sh
@@ -2,18 +2,31 @@
 sudo add-apt-repository -y ppa:xorg-edgers/ppa
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
-sudo apt-get -y install aptitude git subversion vim compizconfig-settings-manager compiz-plugins compiz-plugins-extra
+
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install aptitude git subversion vim compizconfig-settings-manager compiz-plugins compiz-plugins-extra python3
 sudo aptitude -y install nodejs npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+npm config set proxy http://proxy-chain.intel.com:911
 sudo npm install -g -y http-server
 sudo aptitude -y install openssh-server
 sudo aptitude -y install fail2ban
 sudo aptitude -y install indicator-multiload
+sudo aptitude -y install atom
+sudo aptitude -y install python-pip python-pip3
 #sudo apt-get -y install apache2
 #sudo apt-get install mysql-server php5-mysql
 sudo aptitude -y install ipython
 #sudo apt-get install libgnome2-bin
 
+mkdir code
+cd code
+git config --global http.proxy http://proxy-chain.intel.com:911
+git config --global user.email "jonathanbyrn@gmail.com"
+git config --global user.name "squeakus"
+git clone https://github.com/squeakus/bitsandbytes.git
+
+cd bitsandbytes
 cd bashrc/
 sudo make install
 cd ../
