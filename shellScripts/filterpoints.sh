c14689bac67ac2dc21b818aef36164dd487148c1
squeakus <jonathanbyrn@gmail.com>
2017-09-08 16:41:02 +0100

    new filter, this only removes the outliers

--- shellScripts/filterpoints.sh
+++ shellScripts/filterpoints.sh
@@ -3,4 +3,4 @@
 mkdir filtered
 
 find . -maxdepth 1 -name "*.laz" | xargs -n1 -P3 -I fname sh -c 'echo fname; pdal -v 4 pipeline noisefilter.json --writers.las.filename=filtered/fname --readers.las.filename=fname'
-
+/home/jonathan/code/vola/VOLA/vola_api/las2vola.py "filtered/*.laz" 4 --crs 29902 -n 
