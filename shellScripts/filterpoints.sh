ee74c08d765c81b67781ff5664854298a8f02cb0
squeakus <jonathanbyrn@gmail.com>
2017-11-08 17:56:14 +0000

    fixing filterpoints

--- shellScripts/filterpoints.sh
+++ shellScripts/filterpoints.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PROCS=4
+PROCS=1
 DEPTH=3
-CRS=29902
+CRS=2905
 
 mkdir pdalfiltered
 
 find . -maxdepth 1 -name "*.laz" | xargs -n1 -P$PROCS -I fname sh -c 'echo fname; pdal -v 4 pipeline noisefilter.json --writers.las.filename=pdalfiltered/fname --readers.las.filename=fname'
 
 cd pdalfiltered
-
-find . -maxdepth 1 -name "*.laz" | xargs -n1 -P$PROCS -I fname python3 las2vola.py fname $DEPTH --crs $CRS
+mv ../*.py .
+find . -maxdepth 1 -name "*.laz" | xargs -n1 -P$PROCS -I fname python3 las2vola.py fname $DEPTH --crs $CRS -n
 
