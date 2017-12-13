ff073b112f2107ed27aec5ebb16e825b5c32509a
squeakus <jonathanbyrn@gmail.com>
2017-12-13 12:59:05 +0000

    adding ssl commands

--- blenderscripts/gpurender.py
+++ blenderscripts/gpurender.py
@@ -1,11 +1,25 @@
-#./blender -b --python gpurender.py trinity2.blend -o ./out --frame-start 1740 --frame-end 1899 -a 
+#./blender -b --python gpurender.py trinity2.blend -o ./out --frame-start 1740 --frame-end 1899 -a
 
 import bpy
 
 bpy.context.scene.cycles.device = 'GPU'
-bpy.context.user_preferences.system.compute_device_type = 'CUDA'
-bpy.context.user_preferences.system.compute_device = 'CUDA_1'
+
+#Useful debug info
+prefs = bpy.context.user_preferences.addons['cycles'].preferences
+print(prefs.compute_device_type)
+
+for d in prefs.devices:
+    print(d.name)
+
+#2.76-
+#bpy.context.user_preferences.system.compute_device_type = 'CUDA'
+#bpy.context.user_preferences.system.compute_device = 'CUDA_1'
 #bpy.context.user_preferences.system.compute_device = 'CUDA_MULTI_2' # Uncomment for 2 GPUs
+#2.77+
+bpy.context.user_preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
+bpy.context.user_preferences.addons['cycles'].preferences.compute_device = 'CUDA_1'
+#bpy.context.user_preferences.addons['cycles'].preferences.compute_device = 'CUDA_MULTI_2'
+
 bpy.context.scene.render.tile_x = 256
 bpy.context.scene.render.tile_y = 256
 bpy.context.scene.render.resolution_percentage = 100
