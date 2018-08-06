fab8295c6223cc0920bd3cfeab856bf98e6ba7fb
squeakus <jonathanbyrn@gmail.com>
2018-08-06 13:10:59 +0100

    fixing capture to only send during daytime

--- raspicam/takeandsend.sh
+++ raspicam/takeandsend.sh
@@ -3,10 +3,19 @@ mkdir -p /home/pi/images
 while true; do
 	IMGNAME=/home/pi/images/img$(date +"%Y%m%d%H%M%S").jpg
 	echo "capturing image $IMGNAME"
-	raspistill -o $IMGNAME
 	raspistill -w 640 -h 480 -q 100 -o $IMGNAME
-	scp $IMGNAME jonathan@192.168.1.197:images
-	sleep 10
+
+            brightfloat=`convert $IMGNAME -colorspace Gray -format "%[mean]" info:`
+        brightint=${brightfloat%.*}
+        if [ $brightint -lt "10000" ]; then
+            echo ""$IMGNAME its dark now
+            sleep 600
+        else
+            scp $IMGNAME jonathan@192.168.1.197:images
+        sleep 5 
+        fi
+
+
 	rm -f $IMGNAME
 
 done
