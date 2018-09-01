3f0a1b944da5fff0e2736ea3a9b7a53aad3f5df8
squeakus <jonathanbyrn@gmail.com>
2018-09-01 22:45:24 +0100

    merge resize

--- shellScripts/resize.sh
+++ shellScripts/resize.sh
@@ -2,6 +2,7 @@
 list=`ls | grep '.jpg\|.JPG'` 
 for img in $list; do
     echo "resizing $img"
-    mogrify -resize 33% $img
+    # mogrify -resize 33% $img
+    mogrify -resize 640x480! $img # the ! ignores aspect ratio
 done
 
