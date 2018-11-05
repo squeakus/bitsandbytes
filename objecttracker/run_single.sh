661d213fa204f1364ca0dc001240748626efade1
squeakus <jonathanbyrn@gmail.com>
2018-11-05 16:25:02 +0000

    adding object tracking code

--- objecttracker/run_single.sh
+++ objecttracker/run_single.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 track_object.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video input/cat.mp4 --label cat --output output/cat_output.avi
+python3 track_single_object.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video input/cat.mp4 --label cat --output output/cat_output.avi
