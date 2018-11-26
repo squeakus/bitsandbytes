baa86b004e98d9064a167db48d17cc3f6c91a495
squeakus <jonathanbyrn@gmail.com>
2018-11-26 10:33:05 +0000

    helping out with the gui

---
+++ photobooth/onscreenkeyboard.py
@@ -0,0 +1,82 @@
+"""
+Simple on-screen keyboard using tkinter
+Author : Ajinkya Padwad
+Version 1.0
+"""
+
+import tkinter as tk
+from tkinter import Entry
+
+
+def main():
+    kb = OnscreenKeyboard()
+
+    print("hello!")
+    print(kb.username)
+
+
+class OnscreenKeyboard:
+    """
+    touch screen keyboard for handling input on raspiTFT
+    """
+
+    def __init__(self):
+        self.kb = tk.Tk()
+        self.kb.title("Enter User Name:")
+        self.username = ""
+        self.kb.resizable(0, 0)
+        self.entry = Entry(self.kb, width=50)
+        self.entry.grid(row=0, columnspan=15)
+
+        self.buttons = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'BACK',
+                        'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'CANCEL',
+                        'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '_', 'DONE']
+        self.create_keyboard()
+        self.kb.mainloop()
+
+    def select(self, value):
+        """
+        Map buttons to alphanumeric buttons to keypress, handle delete
+        cancel and submit options as well.
+        """
+        if value == "BACK":
+            self.entry.delete(len(self.entry.get())-1, tk.END)
+
+        elif value == "CANCEL":
+            self.kb.destroy()
+        elif value == 'DONE':
+            self.username = self.entry.get()
+            self.kb.destroy()
+        else:
+            self.entry.insert(tk.END, value)
+
+    def create_keyboard(self):
+        """ Add the buttons to a gridlayout 9 wide"""
+        row = 1  # leave room for the text box
+        col = 0
+
+        for button in self.buttons:
+
+            def command(x=button): return self.select(x)
+
+            if button == "CANCEL" or button == "DONE" or button == "BACK":
+                tk.Button(self.kb, text=button, width=6, bg="#3c4987", fg="#ffffff",
+                          activebackground="#ffffff", activeforeground="#3c4987",
+                          relief='raised', padx=1, pady=1, bd=1,
+                          command=command).grid(row=row, column=col)
+
+            else:
+                tk.Button(self.kb, text=button, width=4, bg="#3c4987", fg="#ffffff",
+                          activebackground="#ffffff", activeforeground="#3c4987",
+                          relief='raised', padx=1, pady=1, bd=1,
+                          command=command).grid(row=row, column=col)
+            col += 1
+
+            # 9 buttons per row
+            if col > 9:
+                col = 0
+                row += 1
+
+
+if __name__ == '__main__':
+    main()
