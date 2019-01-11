906a0e233872adc4992b24eb4215f180939108cb
squeakus <jonathanbyrn@gmail.com>
2019-01-11 13:48:55 +0000

    new result parser

--- openvino/result_parser.py
+++ openvino/result_parser.py
@@ -9,14 +9,32 @@ def main():
     experiments = load_result(
         'ncs1ov', 'results/ncs1_ovresult.txt', experiments)
     experiments = load_result(
+        'ncs2ov', 'results/ncs2_ovresult.txt', experiments)
+    experiments = load_result(
         'ncs1ovusb2', 'usb2results/ncs1_ovresult.txt', experiments)
     experiments = load_result(
+        'ncs2ovusb2', 'usb2results/ncs2_ovresult.txt', experiments)
+    experiments = load_result(
         'ncs1sdk', 'results/ncs1_sdkresult.txt', experiments)
+    experiments = load_result(
+        'ncesdk', 'results/nce_sdkresult.txt', experiments)
+
+
     experiments = OrderedDict(sorted(experiments.items()))
-    explist = ['ncs1sdk', 'ncs1ov']
-    headers = ["Network", "NCS1 SDK", "NCS1 OV", 'speedup']
-    explist = ['ncs1ovusb2', 'ncs1sdk', 'speedup']
-    #headers = ["Network", "NCS1 OV"]
+
+    # compare OV and NCSDK
+    explist = ['ncs1sdk', 'ncs1ov', 'speedup', 'ncesdk', 'ncs2ov', 'speedup']
+    headers = ["Network", "NCS1 SDK", "NCS1 OV", 'speedup', "NCE SDK", "NCS2 OV", 'speedup']
+
+    # NCS 1 vs NCS2
+    explist = ['ncs1ov', 'ncs2ov']
+    headers = ["Network", "NCS1 OpenVINO", "NCS2 OpenVINO"]
+
+    # USB3 vs USB 2
+
+    explist = ['ncs1ov', 'ncs1ovusb2', 'speedup', 'ncs2ov', 'ncs2ovusb2', 'speedup']
+    headers = ["Network", "NCS1 USB3", "NCS1 USB2", "slowdown",  "NCS2 USB3", "NCS2 USB2", "slowdown"]
+    
 
     table_results(experiments, explist, headers)
 
@@ -40,20 +58,22 @@ def table_results(experiments, explist, headers):
 
     averages = robust_average(results)
     results.append(['average']+averages)
-    # print(tabulate(results, headers=headers, tablefmt="latex"))
-    print(tabulate(results, headers=headers))
+    print(tabulate(results, headers=headers, tablefmt="latex"))
+    #print(tabulate(results, headers=headers))
 
 
 def robust_average(results):
     cols = len(results[0]) - 1
     res_list = [[] for _ in range(cols)]
     for result in results:
+
         # cut out the network name
         for idx, elem in enumerate(result[1:]):
             if not elem == 'NA':
                 res_list[idx].append(elem)
 
     averages = []
+
     for res in res_list:
         averages.append(round(ave(res), 2))
 
