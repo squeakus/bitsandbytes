6805c4f56a56266d39c84f4976241d6ffff2ed6b
squeakus <jonathanbyrn@gmail.com>
2019-04-08 15:45:45 +0100

    adding voc parser

--- tensorflow/openimages2voc.py
+++ tensorflow/openimages2voc.py
@@ -25,6 +25,19 @@ def write_csv(dataset, name):
             csvfile.write(','.join(map(str, row))+'\n')
 
 
+def write_VOC():
+
+    annotation = ET.Element("annotation")
+    folder = ET.SubElement(annotation, "folder").text = "JPEGImages"
+ET.SubElement(annotation, "folder").text = "JPEGImages"
+
+
+    ET.SubElement(doc, "field1", name="blah").text = "some value1"
+    ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+
+    tree = ET.ElementTree(root)
+    tree.write("filename.xml")
+
 def count_labels(dataset):
     labeldict = {}
 
