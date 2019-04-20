9d2acbd4b52c11afead7f207a363269aa9613a69
squeakus <jonathanbyrn@gmail.com>
2019-04-20 20:14:51 +0100

    updating range

--- facetrack/utils/objcenter.py
+++ facetrack/utils/objcenter.py
@@ -7,15 +7,21 @@ class ObjCenter:
 		# load OpenCV's Haar cascade face detector
 		self.detector = cv2.CascadeClassifier(haarPath)
 		self.color = (0,0, 255)
-		self.testcoord = [0, 120]
+		self.testcoord = [160, 120]
+		self.flip = False
 
 	def update(self, frame, frameCenter):
 		# convert the frame to grayscale
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		
-		self.testcoord[0] += 1
-		if self.testcoord[0] > 320:
-			self.testcoord[0] = 0
+		if self.flip:
+			self.testcoord[0] -= 1
+		else:
+			self.testcoord[0] += 1
+		if self.testcoord[0] > 170:
+			self.flip = True
+		if self.testcoord[0] < 150:
+			self.flip = False
 		print(self.testcoord)
 		return (self.testcoord, (self.testcoord[0], self.testcoord[1], 10, 10))
 		# # detect all faces in the input frame
