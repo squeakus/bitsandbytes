2c2238aac86d56399910604313030d7a020c6d59
squeakus <jonathanbyrn@gmail.com>
2019-04-20 21:55:06 +0100

    now working sensibly

--- facetrack/utils/objcenter.py
+++ facetrack/utils/objcenter.py
@@ -3,47 +3,55 @@ import imutils
 import cv2
  
 class ObjCenter:
-	def __init__(self, haarPath):
-		# load OpenCV's Haar cascade face detector
-		self.detector = cv2.CascadeClassifier(haarPath)
-		self.color = (0,0, 255)
-		self.testcoord = [160, 120]
-		self.flip = False
+    def __init__(self, haarPath):
+        # load OpenCV's Haar cascade face detector
+        self.detector = cv2.CascadeClassifier(haarPath)
+        self.color = (0,0, 255)
+        self.testcoord = [160, 120]
+        self.flip = 0
 
-	def update(self, frame, frameCenter):
-		# convert the frame to grayscale
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		return (320,240, None)
-		# if self.flip:
-		# 	self.testcoord[0] -= 1
-		# else:
-		# 	self.testcoord[0] += 1
-		# if self.testcoord[0] > 170:
-		# 	self.flip = True
-		# if self.testcoord[0] < 150:
-		# 	self.flip = False
-		# print(self.testcoord)
-		# return (self.testcoord, (self.testcoord[0], self.testcoord[1], 10, 10))
-		# # detect all faces in the input frame
-		# rects = self.detector.detectMultiScale(gray, scaleFactor=1.05,
-		# 	minNeighbors=9, minSize=(30, 30),
-		# 	flags=cv2.CASCADE_SCALE_IMAGE)
+    def update(self, frame, frameCenter):
+        # convert the frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # self.flip += 1
+        # if self.flip < 100:
+        #   return ((320,240), None)
+        # elif self.flip < 200:
+        #   return (((160,120), None))
+        # elif self.flip < 300:
+        #   self.flip = 0
+        #   return (((160,120), None))
+        # if self.flip:
+        #   self.testcoord[0] -= 1
+        # else:
+        #   self.testcoord[0] += 1
+        # if self.testcoord[0] > 170:
+        #   self.flip = True
+        # if self.testcoord[0] < 150:
+        #   self.flip = False
+        # print(self.testcoord)
+        # return (self.testcoord, (self.testcoord[0], self.testcoord[1], 10, 10))
+
+        # detect all faces in the input frame
+        rects = self.detector.detectMultiScale(gray, scaleFactor=1.05,
+            minNeighbors=9, minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE)
  
 
+        # check to see if a face was found
+        if len(rects) > 0:
+            self.color = (255, 0, 0)
+            # extract the bounding box coordinates of the face and
+            # use the coordinates to determine the center of the
+            # face
+            (x, y, w, h) = rects[0]
+            faceX = int(x + (w / 2))
+            faceY = int(y + (h / 2))
+            print("face detected at", x,y, "centroid", faceX, faceY)
+            # return the center (x, y)-coordinates of the face
+            return ((faceX, faceY), rects[0])
+ 
+        # otherwise no faces were found, so return the center of the
+        # frame
 
-		# # check to see if a face was found
-		# if len(rects) > 0:
-		# 	self.color = (255, 0, 0)
-		# 	# extract the bounding box coordinates of the face and
-		# 	# use the coordinates to determine the center of the
-		# 	# face
-		# 	(x, y, w, h) = rects[0]
-		# 	faceX = int((x + w) / 2)
-		# 	faceY = int((y + h) / 2)
- 
-		# 	# return the center (x, y)-coordinates of the face
-		# 	return ((faceX, faceY), rects[0])
- 
-		# otherwise no faces were found, so return the center of the
-		# frame
-		return (frameCenter, None)
+        return (frameCenter, None)
