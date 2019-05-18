e2a7273d06382d38deac0783e9e8cb0512e49eae
squeakus <jonathanbyrn@gmail.com>
2019-05-18 22:31:54 +0100

    zsh magic

--- bashrc/installzsh.sh
+++ bashrc/installzsh.sh
@@ -1,4 +1,6 @@
 !#/bin/bash
+sudo apt install zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
