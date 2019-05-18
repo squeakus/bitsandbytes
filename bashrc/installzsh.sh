4000867ea972c5017449743f468d116885fb749f
squeakus <jonathanbyrn@gmail.com>
2019-05-18 22:29:39 +0100

    pimpin my terminal

---
+++ bashrc/installzsh.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#9k shell theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+sed -i '/ZSH_THEME/c\ZSH_THEME="powerlevel9k/powerlevel9k"' ~/.zshrc
+mkdir .local/share/fonts
+mv PowerlineSymbols.otf ~/.local/share/fonts/
+fc-cache -vf ~/.local/share/fonts/
+mkdir .config/fontconfig
+mkdir .config/fontconfig/conf.d
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+echo "plugins+=(git zsh-autosuggestions k  zsh-syntax-highlighting)" >> ~/.zshrc
+MYUSER=`whoami`
+sudo -s
+chsh -s /bin/zsh root
+chsh -s /bin/zsh
+chsh -s /bin/zsh $MYUSER
