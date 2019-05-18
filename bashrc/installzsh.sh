57c93a2414c38cfa1648c49980977daec3eee0f4
squeakus <jonathanbyrn@gmail.com>
2019-05-18 22:43:59 +0100

    updating zsh installer

--- bashrc/installzsh.sh
+++ bashrc/installzsh.sh
@@ -1,19 +1,27 @@
-!#/bin/bash
-sudo apt install zsh
+#!/bin/bash
+sudo apt install -y zsh
+
+# install the plugins
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#9k shell theme
+git clone https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions -~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+#set up powerline fonts and the 9k theme
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-sed -i '/ZSH_THEME/c\ZSH_THEME="powerlevel9k/powerlevel9k"' ~/.zshrc
-mkdir .local/share/fonts
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p .local/share/fonts
 mv PowerlineSymbols.otf ~/.local/share/fonts/
 fc-cache -vf ~/.local/share/fonts/
-mkdir .config/fontconfig
-mkdir .config/fontconfig/conf.d
+mkdir -p .config/fontconfig/conf.d
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+#update .zshrc
+sed -i '/ZSH_THEME/c\ZSH_THEME="powerlevel9k/powerlevel9k"' ~/.zshrc
 echo "plugins+=(git zsh-autosuggestions k  zsh-syntax-highlighting)" >> ~/.zshrc
+
+#set as default shell
 MYUSER=`whoami`
 sudo -s
 chsh -s /bin/zsh root
