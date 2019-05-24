f60e7642ca4d3298b33d02d7c56a6bd69c0602fa
squeakus <jonathanbyrn@gmail.com>
2019-05-24 20:41:43 +0100

    new ping script

---
+++ shellScripts/pingcheck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ping_targets="raspberrypi.local"
+failed_hosts=""
+
+for i in $ping_targets
+do
+   ping -c 1 $i > /dev/null
+   if [ $? -ne 0 ]; then
+      if [ "$failed_hosts" == "" ]; then
+         failed_hosts="$i"
+      else
+         failed_hosts="$failed_hosts, $i"
+      fi
+   fi
+done
+
+if [ "$failed_hosts" != "" ]; then
+   printf "Subject: server down\n\nThe following hosts are down: $failed_hosts" | ssmtp jonathanbyrn@gmail.com
+fi
