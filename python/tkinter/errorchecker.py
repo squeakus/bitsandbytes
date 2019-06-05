d9cead32e3daa813085e07beb9d7853b36803e95
squeakus <jonathanbyrn@gmail.com>
2019-06-05 22:13:38 +0100

    now with button clicking sexiness

--- python/tkinter/errorchecker.py
+++ python/tkinter/errorchecker.py
@@ -4,12 +4,11 @@ from PIL import Image, ImageTk
 
 def main():
 
-    changes = [{'coords':[230, 10, 290, 60], 'type':"false"},
-          {'coords':[400, 100, 450, 130], 'type':"false"},
-          {'coords':[100, 290, 140, 340], 'type':"false"},
-          {'coords':[500, 500, 520, 520], 'type':"false"}]
-    checkerwindow = ErrorChecker('cat.jpg')
-
+    changes = [{'coords': [230, 10, 290, 60], 'type':"false"},
+               {'coords': [400, 100, 450, 130], 'type':"false"},
+               {'coords': [100, 290, 140, 340], 'type':"false"},
+               {'coords': [500, 500, 520, 520], 'type':"false"}]
+    checkerwindow = ErrorChecker('cat.jpg', changes)
 
 
 class ErrorChecker:
@@ -18,31 +17,32 @@ class ErrorChecker:
         self.changes = changes
         self.root = tk.Tk()
         self.img = ImageTk.PhotoImage(Image.open(filename))
-        self.canvas = tk.Canvas(root, bg='black',
-                                width=img.width(), 
-                                height=img.height())
+        self.canvas = tk.Canvas(self.root, bg='black',
+                                width=self.img.width(),
+                                height=self.img.height())
 
         # listen for mouse clicks
-        self.canvas.bind("<Button-1>", mouse_click)
+        self.canvas.bind("<Button-1>", self.mouse_click)
         self.canvas.pack(fill="both", expand="yes")
 
         # load an image
-        self.canvas.create_image(0, 0, image=img, anchor=tk.NW)
+        self.canvas.create_image(0, 0, image=self.img, anchor=tk.NW)
 
         # draw the boxes
         for change in self.changes:
             print(change)
-            draw_box(canvas, change['coords'], 'green3')
-        root.mainloop()
+            draw_box(self.canvas, change['coords'], 'green3')
+        self.root.mainloop()
 
     def mouse_click(self, event):
         print("clicked at", event.x, event.y)
-        for change in changes:
+        for change in self.changes:
             self.check_boundaries(event.x, event.y, change['coords'])
 
     def check_boundaries(self, x, y, coords):
         if x > coords[0] and x < coords[2]:
             if y > coords[1] and y < coords[3]:
+                print("clicked on a box")
                 draw_box(self.canvas, coords, 'blue')
 
 
@@ -51,7 +51,7 @@ def draw_box(canvas, coords, color):
                             coords[2], coords[3],
                             outline=color,
                             width=2)
-    
+
 
 if __name__ == "__main__":
     main()
