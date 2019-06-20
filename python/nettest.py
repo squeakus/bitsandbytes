b710cede28701c9aa0a5ccb41163cad4181f25de
squeakus <jonathanbyrn@gmail.com>
2019-06-20 16:13:01 +0100

    adding nettest

---
+++ python/nettest.py
@@ -0,0 +1,20 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+from networkx.drawing.nx_agraph import graphviz_layout
+
+
+adj = np.array([[0,1,0,0,1,0,0,0,0,0],
+				[0,0,1,0,0,1,0,0,0,0],
+				[0,0,0,1,0,0,1,0,0,0],
+				[0,0,0,0,0,0,0,1,0,0],
+				[0,0,0,0,0,0,0,0,1,1],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0]])
+G = nx.from_numpy_matrix(adj, nx.DiGraph())
+pos=graphviz_layout(G, prog='dot')
+nx.draw_networkx(G, with_labels=True)
+plt.show()
