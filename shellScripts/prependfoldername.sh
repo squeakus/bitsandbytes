de7202f20ae0735d482571ca37ed6b104e412600
squeakus <jonathanbyrn@gmail.com>
2019-06-30 13:18:10 +0100

    finally got prepend working

--- shellScripts/prependfoldername.sh
+++ shellScripts/prependfoldername.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 mkdir -p alllabels
 mkdir -p all
-# images=`find . -name ! -path ./all "*.jpg"`
-# for f in $images; 
-# do 
-# 	newname=`echo $f | sed 's/\///' | sed 's/.//' | sed 's/\//_/'`
-# 	cp "$f" "all/$newname"; 
-# done
+images=`find . -name "*.jpg" ! -path ./all`
+
+for f in $images; 
+do 
+	newname=`echo $f | sed 's/\///' | sed 's/.//' | sed 's/\//_/'`
+	echo "copying $newname"
+	cp "$f" "all/$newname"; 
+done
 
 labels=`find . -name "*.xml" ! -path ./alllabels`
 for f in $labels; 
 do 
 	newname=`echo $f | sed 's/\///' | sed 's/.//' | sed 's/\//_/'`
 	echo "copying $f to alllabels/$newname"
-	cp "$f" "alllabels/$newname"; 
+	cp "$f" "alllabels/$newname";
+	fname=$(basename "${f%.*}")
+	newfname=${newname%.*}
+	echo "sed -i 's/$fname/$newfname/g' alllabels/$newname"
+	sed -i "s/$fname/$newfname/g" "alllabels/$newname"
 done
 
