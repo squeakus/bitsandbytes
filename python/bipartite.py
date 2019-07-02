fa3a69d874034b8b500ad614ac79a7cd4a873943
squeakus <jonathanbyrn@gmail.com>
2019-07-02 11:18:18 +0100

    new examples flying

--- python/bipartite.py
+++ python/bipartite.py
@@ -15,11 +15,11 @@ adj = np.array([[0,0,0,0,0,1,1,1,1,1],
 				[0,0,0,0,0,0,0,0,0,0],
 				[0,0,0,0,0,0,0,0,0,0]])
 
-G = nx.bipartite.gnmk_random_graph(3, 5, 10, seed=123)
+G = nx.bipartite.gnmk_random_graph(5, 5, 25, seed=123)
 top = nx.bipartite.sets(G)[0]
-#pos = nx.bipartite_layout(G, top)
+pos = nx.bipartite_layout(G, top)
 
 #G = nx.from_numpy_matrix(adj, nx.DiGraph())
 # nx.write_gexf(G, "test.gexf")
-nx.draw_networkx(G, with_labels=True)
+nx.draw_networkx(G, pos, with_labels=True)
 plt.show()
