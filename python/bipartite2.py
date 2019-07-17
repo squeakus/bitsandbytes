0b7a6ca1f4345d8704717c35e14c4d8a8b0035bc
Byrne <jonathan.byrne@intel.com>
2019-07-17 20:35:21 +0100

    new graph function

---
+++ python/bipartite2.py
@@ -0,0 +1,25 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+from networkx.drawing.nx_agraph import graphviz_layout
+from networkx.algorithms import bipartite
+
+adj = np.array([[0,0,0,0,0,1,1,1,1,1],
+				[0,0,0,0,0,1,1,1,1,1],
+				[0,0,0,0,0,1,1,1,1,1],
+				[0,0,0,0,0,1,1,1,1,1],
+				[0,0,0,0,0,1,1,1,1,1],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0]])
+
+G = nx.bipartite.gnmk_random_graph(3, 5, 10, seed=123)
+top = nx.bipartite.sets(G)[0]
+#pos = nx.bipartite_layout(G, top)
+
+#G = nx.from_numpy_matrix(adj, nx.DiGraph())
+# nx.write_gexf(G, "test.gexf")
+nx.draw_networkx(G, with_labels=True)
+plt.show()
