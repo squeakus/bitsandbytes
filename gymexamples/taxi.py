c3fe2ffdc282deb4b8164ab93520e60ccb9b736d
squeakus <jonathanbyrn@gmail.com>
2019-07-22 18:28:56 +0100

    new gym examples

---
+++ gymexamples/taxi.py
@@ -0,0 +1,46 @@
+import gym
+
+
+env = gym.make("Taxi-v2").env
+
+env.render()
+env.s = 328  # set environment to illustration's state
+
+epochs = 0
+penalties, reward = 0, 0
+
+frames = [] # for animation
+
+done = False
+
+while not done:
+    action = env.action_space.sample()
+    state, reward, done, info = env.step(action)
+
+    if reward == -10:
+        penalties += 1
+    
+    # Put each rendered frame into dict for animation
+    frames.append({
+        'frame': env.render(mode='ansi'),
+        'state': state,
+        'action': action,
+        'reward': reward
+        }
+    )
+
+    epochs += 1
+    
+def print_frames(frames):
+    for i, frame in enumerate(frames):
+        clear_output(wait=True)
+        print(frame['frame'].getvalue())
+        print(f"Timestep: {i + 1}")
+        print(f"State: {frame['state']}")
+        print(f"Action: {frame['action']}")
+        print(f"Reward: {frame['reward']}")
+        sleep(.1)
+        
+print_frames(frames)
+print("Timesteps taken: {}".format(epochs))
+print("Penalties incurred: {}".format(penalties))
