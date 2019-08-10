ebb58fda1c7b4d33f979aab88b876840847ec416
squeakus <jonathanbyrn@gmail.com>
2019-08-10 19:37:36 +0100

    new tweepy search

--- python/twitter/config_example.py
+++ python/twitter/config_example.py
@@ -5,3 +5,5 @@ consumer_key = ''
 consumer_secret = ''
 access_key = ''
 access_secret = ''
+access_token = access_key
+access_token_secret = access_secret
---
+++ python/twitter/tweetsearch.py
@@ -0,0 +1,31 @@
+import tweepy
+import csv
+import pandas as pd
+from config import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
+
+
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+#####United Airlines
+# Open/Create a file to append data
+csvFile = open('garda.csv', 'a')
+#Use csv Writer
+csvWriter = csv.writer(csvFile)
+
+for tweet in tweepy.Cursor(api.search,q="garda",count=100,
+                           lang="en",
+                           since="2017-04-03").items():
+    print (tweet.created_at, tweet.text)
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
