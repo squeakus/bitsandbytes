331e3ef3197bd70d45a6ae166973488c54847b12
squeakus <jonathanbyrn@gmail.com>
2019-10-02 19:38:36 +0100

    Now working with sims asnd at commands

--- opencv/contours.py
+++ opencv/contours.py
@@ -1,12 +1,12 @@
 import numpy as np
 import cv2
 
-im = cv2.imread('star.jpg')
+im = cv2.imread('test.png')
 imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(imgray,127,255,0)
-contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+_, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-print "contours", len(contours), "boundary points", len(contours[0])
+print("contours", len(contours), "boundary points", len(contours[0]))
 
 #line 3 pixels wide
 cv2.imshow('image',im)
