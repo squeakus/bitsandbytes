797343806178d68000c032b8bba6c70d372320ea
squeakus <jonathanbyrn@gmail.com>
2019-12-04 12:37:57 +0000

    adding scripts for setting environment

---
+++ bashrc/set_proxy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export HTTP_PROXY=http://proxy-ir.intel.com:911
+export http_proxy=http://proxy-ir.intel.com:911
+export HTTPS_PROXY=http://proxy-ir.intel.com:912
+export https_proxy=http://proxy-ir.intel.com:912
+export FTP_PROXY=http://proxy-ir.intel.com:911
+export ftp_proxy=http://proxy-ir.intel.com:911
+export SOCKS_PROXY=http://proxy-ir.intel.com:1080
+export socks_proxy=http://proxy-ir.intel.com:1080
+export NO_PROXY=intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,.local,127.0.0.0/8,134.134.0.0/16
+export no_proxy=intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,.local,127.0.0.0/8,134.134.0.0/16
