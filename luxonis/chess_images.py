27b3c34f6498a8ba36a1af21de65fd973aba4d69
byrnejon <jonathan.byrne@intel.com>
2020-04-30 17:19:17 +0100

    wrapping in main, removing hacky framecount, adding text to notify user of failed frame capture

--- luxonis/chess_images.py
+++ luxonis/chess_images.py
@@ -16,7 +16,8 @@ import glob
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
+chessboard_flags = None
+#chessboard_flags = cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
@@ -32,7 +33,7 @@ for fname in images:
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),chessboard_flags)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -43,7 +44,7 @@ for fname in images:
 
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+    cv2.imshow(fname,img)
+    cv2.waitKey(0)
 
 
