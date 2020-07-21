34542ea336e0c5ccdd3a22984313ae23a0b5e6f2
squeakus <jonathanbyrn@gmail.com>
2020-07-21 10:27:03 +0100

    fixing joystick inputs

--- robomow/robomow.ino
+++ robomow/robomow.ino
@@ -1,9 +1,9 @@
 //Jonathan Byrne robo mower
 
-#define LEFT_FORWARD    5
-#define LEFT_BACK    3
-#define RIGHT_FORWARD    9
-#define RIGHT_BACK    6
+#define RIGHT_FORWARD    5
+#define RIGHT_BACK    3
+#define LEFT_FORWARD    9
+#define LEFT_BACK    6
 
 void setup() {
   //monitor logger and motor driver
@@ -14,7 +14,48 @@ void setup() {
   pinMode(9, OUTPUT);
 }
 
+void motor_left(int power){
+  if (power > 0){
+    Serial.println("left_forward");
+    analogWrite(LEFT_FORWARD, power);
+    analogWrite(LEFT_BACK, 0);
+  } 
+  else {
+    Serial.println("left_backward");
+    analogWrite(LEFT_FORWARD, 0);
+    analogWrite(LEFT_BACK, abs(power));
+  }
+}
+
+void motor_right(int power){
+  if (power > 0){
+    Serial.println("right_forward");
+    analogWrite(RIGHT_FORWARD, power);
+    analogWrite(RIGHT_BACK, 0);
+  } 
+  else {
+    Serial.println("right_backward");
+    analogWrite(RIGHT_FORWARD, 0);
+    analogWrite(RIGHT_BACK, abs(power));
+  }
+}
+
+void joystick_read(){
+  int x = analogRead(A0); 
+  int y = analogRead(A1);  
+  Serial.println("x:"+String(x)+" y:"+String(y));          // debug value
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  joystick_read();
+  delay(50);
+//    motor_left(100);
+//    delay(5000);
+//    motor_left(-100);
+//    delay(5000);
+//    motor_right(100);
+//    delay(5000);
+//    motor_right(-100);
+//    delay(5000);
 
 }
