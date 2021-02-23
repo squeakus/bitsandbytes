c9a88a0006c12ef0fcdfb27b1812efa1691e855d
squeakus <jonathanbyrn@gmail.com>
2021-02-23 21:22:22 +0000

    done

--- vtk/sliderbar_vista.py
+++ vtk/sliderbar_vista.py
@@ -5,13 +5,17 @@ mesh = examples.download_knee_full()
 
 
 p = pv.Plotter(shape=(1, 2))
+
 # opacity = [0, 0, 0, 0.1, 0.3, 0.6, 1]
 p.add_volume(mesh, cmap="bone", opacity="sigmoid")
+# p.add_mesh_slice(
+#     mesh,
+# )
 p.subplot(0, 1)
 
 # p.show()
 
 # p1 = pv.Plotter()
 p.add_mesh_slice_orthogonal(mesh)
-p.link_views()
+# p.link_views()
 p.show()
