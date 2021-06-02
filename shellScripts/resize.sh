a8bb5f1202e99d353f5241c0b095e4df67cf0e6a
byrnejon <jonathan.byrne@intel.com>
2021-06-02 09:16:56 +0100

    more commands

--- shellScripts/resize.sh
+++ shellScripts/resize.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo "HELLO!"
+
 list=`ls | grep '.jpg\|.JPG\|.png\|.PNG\|.tif\|.TIF'` 
 for img in $list; do
     echo "resizing $img"
     # mogrify -resize 33% $img
     # mogrify -resize 100x100 $img # the ! ignores aspect ratio
-    mogrify -negate $img
+    mogrify -resize 1000x1000! $img
 done
 
