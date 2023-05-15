c061830147926db601b33fc4e2b8388a5c99161a
squeakus <jonathanbyrn@gmail.com>
2023-05-15 19:34:59 +0100

    cleaning script

---
+++ openai/clean_transcription.py
@@ -0,0 +1,37 @@
+import nltk
+import webvtt
+from nltk.corpus import stopwords
+
+# Download stopwords if necessary
+nltk.download("stopwords")
+
+# Define list of filler words
+filler_words = ["um", "uh", "like", "you know", "ah"]
+
+# Define path to input and output files
+input_file_path = "test_vid.vtt"
+output_file_path = "test_vid_clean.txt"
+
+# Open input file and read contents
+with open(input_file_path, "r") as input_file:
+    vtt_content = input_file.read()
+
+clean_list = []
+for caption in webvtt.read(input_file_path):
+    clean_list.append(caption.text)
+
+text_content = " ".join(clean_list)
+
+# Tokenize text content
+tokens = nltk.word_tokenize(text_content)
+
+# Remove filler words
+stop_words = set(stopwords.words("english")).union(filler_words)
+filtered_tokens = [token for token in tokens if token.lower() not in stop_words]
+
+# Join filtered tokens back into a single string
+filtered_text = " ".join(filtered_tokens)
+
+# Write cleaned text to output file
+with open(output_file_path, "w") as output_file:
+    output_file.write(filtered_text)
