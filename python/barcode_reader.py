37e94c889f996a6e1c84f1664b9bb879c94cf590
squeakus <jonathanbyrn@gmail.com>
2023-06-08 21:44:58 +0100

    barcode reader

---
+++ python/barcode_reader.py
@@ -0,0 +1,41 @@
+import os
+
+import keyboard
+
+# Function to write barcode data to the file
+
+
+def write_to_file(data):
+    with open("out.txt", "a") as f:
+        f.write(data + "\n")
+
+
+# Function to read barcode scanner input
+
+
+def read_barcode_scanner_input():
+    barcode_data = ""
+
+    while True:
+        event = keyboard.read_event()
+
+        if event.event_type == keyboard.KEY_DOWN:
+            if event.name == "esc":
+                break
+
+            elif event.name == "enter":
+                print(f"scan: {barcode_data}")
+                write_to_file(barcode_data)
+
+                barcode_data = ""
+
+        else:
+            barcode_data += str(event.name)
+
+
+if __name__ == "__main__":
+    print("Start scanning barcodes (press 'esc' to quit):")
+
+    read_barcode_scanner_input()
+
+    print("Exiting the program...")
